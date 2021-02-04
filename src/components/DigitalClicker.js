af002a9fbf86d150a4ff5a746dbca055faff4d54
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-02-03 21:44:40 -0500

    commit tree 7b546143872d4f50b6391bf8b2520241b2060b3c

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
 // Code DigitalClicker Component Here
+
+import React, {Component} from 'react'
+
+class DigitalClicker extends Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  updateClicked = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+
+  render(){
+    return(
+      <button onClick={this.updateClicked}>{this.state.timesClicked}</button>
+    )
+  }
+}
+
+export default DigitalClicker;
